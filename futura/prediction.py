e9ada385cfa54486e14aeef836f3aa4b99f6214c
Andrew Jason <107207708+MysticPl4y3r@users.noreply.github.com>
2024-03-17 12:57:59 +0530

    Delete futura directory - forgot to rename to alphabyte

--- futura/prediction.py
+++
@@ -1,9 +0,0 @@
-import joblib
-
-def predict(data):
-    model=joblib.load("./model/best_model.sav")
-    return model.predict(data)
-
-def predict_prob(x_test):
-    model=joblib.load("./model/best_model.sav")
-    return model.predict_proba(x_test)
